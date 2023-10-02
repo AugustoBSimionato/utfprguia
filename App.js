dd933db5188cd401f1276fe89fc5760652563f33
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-10-02 15:25:23 -0300

    adding initial assets

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, SafeAreaView, StatusBar } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar />
+      <Text>Board principal</Text>
+      <Text>Lista para acessar os links</Text>
+    </SafeAreaView>
   );
 }
 
