409e34bbc00bf0866076b6bbd156f756740d7e9e
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2023-10-31 18:26:22 -0300

    feat: adding statusbar good appearance

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import List from './src/components/ListHome';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <StatusBar />
+      <StatusBar backgroundColor="#fff" barStyle="dark-content" />
       <Image source={require("./assets/utfpr-logo-home.png")} style={{width: 200, height: 100, resizeMode: 'contain'}} />
       <Board />
       <List />
